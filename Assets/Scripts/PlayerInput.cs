6c0751f66697f1de5a683f7fd5f91505b699d3b3
justynsmyth <jbusmith@umich.edu>
2025-01-17 12:21:40 -0500

    Finish Snap to Grid functionality

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,106 @@
+using System;
+using UnityEngine;
+
+public class PlayerInput  : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float movement_speed = 4f;
+    
+    void Start()
+    {
+        // Sets the Player Rigidbody to the rb variable
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use .invoke() to publish/trigger a message to subscribed classes
+    public static event Action OnSpacePressed;
+    public static event Action OnXPressed;
+    public static event Action OnZPressed;
+    
+    // Subscribing to an Event via '+='
+    // PlayerInput.OnSpacePressed += FunctionNameHere()
+    // PlayerInput.OnXPressed += () => Debug.Log("X Was Pressed"); // Lambda
+    // Unsubscribing on Destruction via '-='
+    
+    void Update()
+    {
+        HandleInputMovement();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke();
+            Debug.Log("Space Key Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            OnXPressed?.Invoke();
+            Debug.Log("X Key Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            OnZPressed?.Invoke();
+            Debug.Log("Z Key Pressed");
+        }
+    }
+
+    private void HandleInputMovement()
+    {
+        if (!RoomTransition.IsTransitionInProgress())
+        {
+            Vector2 currentInput = GetInput();
+            Vector3 currentVelocity = Vector2.zero;
+            Vector2 currentPos = rb.position;
+
+            if (currentInput.x != 0)
+            {
+                currentVelocity.x += currentInput.x * movement_speed;
+                currentVelocity.y += AlignToGrid(currentPos.y, 0.5f) / Time.fixedDeltaTime;
+            } else if (currentInput.y != 0)
+            {
+                currentVelocity.x += AlignToGrid(currentPos.x, 0.5f) / Time.fixedDeltaTime;
+                currentVelocity.y += currentInput.y * movement_speed;
+            }
+            rb.linearVelocity = currentVelocity; 
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+    
+    /// <summary>
+    /// Takes a position and adjusts it to align with a factor value.
+    /// </summary>
+    /// <param name="val">X or Y position of a player</param>
+    /// <param name="factor">Expected Grid Size to 'Snap' to</param>
+    private float AlignToGrid(float val, float factor)
+    {
+        float r = val % factor;
+        float h = factor / 2;
+        if (r < h)
+        {
+            return -r;
+        }
+
+        return factor - r;
+    }
+    Vector2 GetInput()
+    {
+        // Take horizontal movement inputs (A, D, <-, ->)
+        float horizontal_input = Input.GetAxisRaw("Horizontal");
+
+        // Take vertical movement inputs (W, S, up, down)
+        float vertical_input = Input.GetAxisRaw("Vertical");
+
+        // Remove diagonal movement
+        // If we're moving horizontally, don't allow vertical movement
+        if (Mathf.Abs(horizontal_input) > 0.0f)
+        {
+            vertical_input = 0.0f;
+        }
+
+        return new Vector2(horizontal_input, vertical_input);
+    }
+}
---
+++ Assets/Scripts/RoomTransition.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    private Camera cam;
+    public float transitionTime = 2.5f;
+
+    public float cameraDistanceY = 11f;
+    public float cameraDistanceX = 16f;
+    public float playerAutoWalkDistance = 2f;
+    
+    // used to inform ArrowKeyMovement.cs to stop moving during transition
+    private static bool isTransitionInProgress = false;
+    
+    public static bool IsTransitionInProgress()
+    {
+        return isTransitionInProgress;
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+    
+    public Direction direction;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main; 
+        }
+    }
+    
+    /// <summary>
+    /// Uses the playerAnimator's face direction to determine which room the player intends to enter.
+    /// Since the player overlaps the returning trigger when entering the next room, IsOppositeDirection will
+    /// prevent retriggering a camera transition.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            InputToAnimator playerAnimator = other.GetComponent<InputToAnimator>();
+            Direction playerDirection = playerAnimator.GetPlayerDirection();
+
+            if (IsOppositeDirection(playerDirection))
+            {
+                return;
+            }
+            
+            rb.linearVelocity = Vector3.zero;
+
+            StartCoroutine(TransitionCameraAndMovePlayer(direction, other.gameObject));
+        }    
+    }
+
+    /// <summary>
+    /// Determine if a trigger's set camera transition direction is opposite to player's sprite direction
+    /// </summary>
+    /// <param name="playerDirection"></param>
+    /// <returns></returns>
+    private bool IsOppositeDirection(Direction playerDirection)
+    {
+        if (direction == Direction.Right) { return playerDirection == Direction.Left; }
+        if (direction == Direction.Left) { return playerDirection == Direction.Right; }
+        if (direction == Direction.Up) { return playerDirection == Direction.Down; }
+        if (direction == Direction.Down) { return playerDirection == Direction.Up; }
+        return false;
+    }
+
+    private IEnumerator TransitionCameraAndMovePlayer(Direction moveDirection, GameObject player)
+    {
+        isTransitionInProgress = true; 
+        Vector3 startPos = cam.transform.position;
+        Vector3 endPos = startPos + GetDirectionVector(moveDirection);
+
+        yield return StartCoroutine(
+            MoveObjectOverTime(
+                cam.transform, startPos, endPos, transitionTime)
+        );
+        
+        yield return StartCoroutine(MovePlayerForwardForDuration(player, moveDirection, 0.5f));
+
+        isTransitionInProgress = false;
+    }
+    
+    /// <summary>
+    /// Translates the player's position (playerAutoWalkDistance) in the moveDirection via linear interpolation.
+    /// Chose to use player transform over linearVelocity because
+    /// the player would have a small tendency to get stuck during transition for some reason.
+    /// </summary>
+    private IEnumerator MovePlayerForwardForDuration(GameObject player, Direction moveDirection, float duration)
+    {
+        Transform playerTransform = player.transform;
+    
+        Vector3 movement = moveDirection switch
+        {
+            Direction.Up => Vector3.up * playerAutoWalkDistance,
+            Direction.Down => Vector3.down * playerAutoWalkDistance,
+            Direction.Left => Vector3.left * playerAutoWalkDistance,
+            Direction.Right => Vector3.right * playerAutoWalkDistance,
+            _ => Vector3.zero
+        };
+
+        Vector3 targetPosition = playerTransform.position + movement;
+
+        float startTime = Time.time;
+        Vector3 startPosition = playerTransform.position;
+
+        while (Time.time - startTime < duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            playerTransform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            yield return null;
+        }
+        playerTransform.position = targetPosition;
+    }
+ 
+
+
+    private Vector3 GetDirectionVector(Direction moveDirection)
+    {
+        switch (moveDirection)
+        {
+            case Direction.Up:
+                return new Vector3(0, cameraDistanceY, 0);
+            case Direction.Down:
+                return new Vector3(0, -cameraDistanceY, 0);
+            case Direction.Left:
+                return new Vector3(-cameraDistanceX, 0, 0);
+            case Direction.Right:
+                return new Vector3(cameraDistanceX, 0, 0);
+            default:
+                return new Vector3(0, 0, 0);
+        }
+    }
+
+    IEnumerator MoveObjectOverTime(Transform t, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float startTime = Time.time;
+        float progress = (Time.time - startTime) / time;
+        while (progress < 1.0f)
+        {
+            progress = (Time.time - startTime) / time;
+            Vector3 pos = Vector3.Lerp(startPos, endPos, progress);
+            
+            t.position = pos;
+            yield return null;
+        }
+        
+        t.position = endPos;
+    }
+}
