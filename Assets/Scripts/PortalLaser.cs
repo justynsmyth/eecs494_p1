0c746b975bb403720917181eaa44cbc162009b44
caidenfung <cafung@umich.edu>
2025-02-08 13:30:18 -0500

    door tag change

--- Assets/Scripts/PortalLaser.cs
+++ Assets/Scripts/PortalLaser.cs
@@ -16,7 +16,7 @@ public class PortalLaser : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {        
         // check for collisions with objects of default or block layer
-        if ((collision.gameObject.layer == 0 || collision.gameObject.layer == 6) && !used)
+        if ((collision.gameObject.layer == 0) && !used)
         {
             Debug.Log("Collided with wall");
             // create a portal
